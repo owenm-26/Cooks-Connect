b45d5e1a48bb2d0a183eb643cb362c108bf58b95
Owen Mariani <mariani.owen@gmail.com>
2024-02-22 13:13:41 -0500

    initialized vite project and added readme instructions

---
+++ cooks-connect/README.md
@@ -0,0 +1,3 @@
+# Instructions
+1. `npm run dev` within <mark>/cooks-connect</mark>
+2. Populate <mark>.env</mark> with credentials using template <mark>.env.example</mark>
---
+++ cooks-connect/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import './assets/chef-hat-logo.png' 
+function App() {
+
+  return (
+    <>
+      <img src='https://cdn-icons-png.flaticon.com/512/4253/4253632.png' alt="logo" width='200px'></img>
+      <h1>Cooks Connect</h1>
+      <p>To be implemented soon...</p>
+    </>
+  )
+}
+
+export default App
