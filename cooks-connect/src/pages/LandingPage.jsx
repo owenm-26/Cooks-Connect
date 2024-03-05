2bb546ba6db1c0d199de9b5fe2981fe9662c96d5
Owen Mariani <mariani.owen@gmail.com>
2024-03-05 17:09:41 -0500

    logo now spins on click

--- cooks-connect/src/pages/LandingPage.jsx
+++ cooks-connect/src/pages/LandingPage.jsx
@@ -1,13 +1,25 @@
 import CustomHeader from "../layout/Header";
+import { useState } from "react";
 
 const LandingPage = () => {
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  const toggleSpin = () => {
+    setIsSpinning(!isSpinning);
+    if (!isSpinning) {
+      setTimeout(() => setIsSpinning(false), 1000); // Adjust the time as needed
+    }
+  };
+
   return (
     <>
       <CustomHeader />
       <img
         src="https://cdn-icons-png.flaticon.com/512/4253/4253632.png"
+        className={isSpinning ? "spin" : ""}
         alt="logo"
         width="200px"
+        onClick={toggleSpin}
       ></img>
       <h1>Cooks Connect</h1>
       <p>To be implemented soon...</p>
