f6abfaafd3d2667eece113ead562d23668e796c0
Owen Mariani <mariani.owen@gmail.com>
2024-03-04 23:13:49 -0500

    made api call

--- cooks-connect/src/App.jsx
+++ cooks-connect/src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
 import './assets/chef-hat-logo.png' 
+import Recipes from './Recipes'
 function App() {
+
+  const renderList = () =>{
+   console.log('worked')
+   
+  }
 
   return (
     <>
       <img src='https://cdn-icons-png.flaticon.com/512/4253/4253632.png' alt="logo" width='200px'></img>
       <h1>Cooks Connect</h1>
       <p>To be implemented soon...</p>
+      <div>
+        <button onClick={renderList}>Click me</button>
+      </div>
+      <Recipes/>
     </>
   )
 }
