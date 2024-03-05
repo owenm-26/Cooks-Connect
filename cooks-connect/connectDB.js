5b698e65aa486f2b0b1111cba33408114aee31f8
Owen Mariani <mariani.owen@gmail.com>
2024-03-04 22:19:19 -0500

    fixed db con with .env

--- cooks-connect/connectDB.js
+++ cooks-connect/connectDB.js
@@ -1,7 +1,17 @@
-var pg = require('pg'); 
-const connection_url = "postgres://steven@localhost:5432/cs411"; 
+import dotenv from 'dotenv';
+import pg from "pg" 
+dotenv.config();
 
-var client = new pg.Client(connection_url); 
+const user = process.env.USERNAME
+const use = 'postgres'
+const port = process.env.LOCAL_HOST
+const db = process.env.DATABASE
+const pass = process.env.PASSWORD
+
+var conString = `postgres://${use}:${pass}@localhost:${port}/${db}`;
+const connection_url = `postgres://${user}@localhost:${port}/${db}`; 
+
+const client = new pg.Client(conString); 
 
 client.connect().then( 
     console.log("Connected.") 
