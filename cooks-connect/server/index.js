b54760ecd471ec11e6169059c5d01236ddf95736
stevenlt <steventranschool132@gmail.com>
2024-04-19 13:09:33 -0400

    mongo setup

---
+++ cooks-connect/server/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+mongoose 
+    .connect("mongodb+srv://steventran:steventran132@cluster0.kz7frgd.mongodb.net/")
+    .then(() => {
+        console.log("alive");
+    })
+    .catch((err) => {
+        console.log('Could not connect to MongoDB and start the server');
+        console.log(err);
+    });
