971855b9ccd52af96760661670240d63d4533960
stevenlt <steventranschool132@gmail.com>
2024-04-22 12:23:07 -0400

    mongo fix

--- cooks-connect/server/index.js
+++ cooks-connect/server/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 mongoose 
     .connect("mongodb+srv://steventran:steventran132@cluster0.kz7frgd.mongodb.net/")
     .then(() => {
-        console.log("alive");
+        console.log('Connected');
     })
     .catch((err) => {
         console.log('Could not connect to MongoDB and start the server');
--- cooks-connect/vite.config.js
+++ cooks-connect/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config() // load env vars from .env
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
