4f1e4bc2982133a4f16dddbffa95c9f8be5e1330
Owen Mariani <mariani.owen@gmail.com>
2024-04-24 14:27:49 -0400

    fixed gitignore and added security

--- cooks-connect/server/index.js
+++ cooks-connect/server/index.js
@@ -1,11 +1,14 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose 
-    .connect("mongodb+srv://steventran:steventran132@cluster0.kz7frgd.mongodb.net/")
-    .then(() => {
-        console.log('Connected');
-    })
-    .catch((err) => {
-        console.log('Could not connect to MongoDB and start the server');
-        console.log(err);
-    });
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => {
+    console.log("Could not connect to MongoDB and start the server");
+    console.log(err);
+  });
