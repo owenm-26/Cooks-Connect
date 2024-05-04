ee34a2c0007ab6fcecf649b10da897f1859b866c
Owen Mariani <mariani.owen@gmail.com>
2024-05-03 22:36:18 -0400

    setup router and controller

--- cooks-connect/server/index.js
+++ cooks-connect/server/index.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const router = require("./router"); // Import the router correctly
 require("dotenv").config();
+
+const app = express();
+app.use(cors());
 
 const MONGO_URI = process.env.MONGO_URI;
 
@@ -12,3 +18,11 @@ mongoose
     console.log("Could not connect to MongoDB and start the server");
     console.log(err);
   });
+
+app.use("/api", router);
+
+// Start your server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
