719d4cccb9dd17e0421809b669ca4bd36130cd8c
Owen Mariani <mariani.owen@gmail.com>
2024-05-03 22:49:24 -0400

    hitting dummy backend successfully

--- cooks-connect/server/router.js
+++ cooks-connect/server/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/joinCommunity", async (req, res) => {
-  res.send({ status: 200, message: "got req" });
+  res.send({ status: 200, message: "got req", req: req.body });
   return;
 });
 
