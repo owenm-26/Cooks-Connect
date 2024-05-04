c1a7e248fa3c47df6db55a971334fdabe124f9b0
Owen Mariani <mariani.owen@gmail.com>
2024-05-04 00:00:55 -0400

    added weather api

--- cooks-connect/client/src/pages/Landing/LandingPage.jsx
+++ cooks-connect/client/src/pages/Landing/LandingPage.jsx
@@ -1,11 +1,17 @@
 import CustomHeader from "../../layout/Header";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./LandingPage.css";
 import { Container, Row, Button } from "react-bootstrap";
 import CustomFooter from "../../layout/Footer";
+import { theme } from "antd";
 
 const LandingPage = () => {
   const [isSpinning, setIsSpinning] = useState(false);
+  const [weatherConditions, setWeatherConditions] = useState();
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
   const toggleSpin = () => {
     setIsSpinning(!isSpinning);
@@ -13,6 +19,30 @@ const LandingPage = () => {
       setTimeout(() => setIsSpinning(false), 1000); // Adjust the time as needed
     }
   };
+
+  useEffect(() => {
+    //weather api
+    const url =
+      "https://yahoo-weather5.p.rapidapi.com/weather?location=boston&format=json&u=f";
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": import.meta.env.VITE_WEATHER_KEY,
+        "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com",
+      },
+    };
+    const getWeather = async () => {
+      const response = await fetch(url, options);
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data.current_observation.condition);
+        setWeatherConditions(data.current_observation.condition);
+        return;
+      }
+    };
+    getWeather();
+  }, []);
 
   return (
     <>
@@ -26,7 +56,27 @@ const LandingPage = () => {
       ></img>
       <div className="intro">
         <h1>Cooks Connect</h1>
-        <p>To be implemented soon...</p>
+        {weatherConditions ? (
+          <div
+            style={{
+              backgroundColor: colorBgContainer,
+              color: "black",
+              width: "20vw",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "0 37vw",
+              borderRadius: "10px",
+              border: "dashed black",
+            }}
+          >
+            <h3>{weatherConditions.text}</h3>
+            <p>{weatherConditions.temperature}</p>
+          </div>
+        ) : (
+          <p>Fetching weather...</p>
+        )}
         <div className="buttonContainer">
           <a href="/login">
             <Button size="lg" className="landingbutton">
@@ -44,7 +94,14 @@ const LandingPage = () => {
           </a>
         </div>
       </div>
-      <div style={{ position: "absolute", bottom: 0, width: "100%" }}>
+      <div
+        style={{
+          maringTop: "20vh",
+          position: "relative",
+          bottom: 0,
+          width: "100%",
+        }}
+      >
         <CustomFooter />
       </div>
     </>
